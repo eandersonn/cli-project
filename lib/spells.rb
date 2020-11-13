587f1b2688052f7e87d61aeb1a5af8d584879dac
Erin Anderson <e.anderson1226@gmail.com>
2020-11-13 18:25:39 -0500

    require gem

---
+++ lib/spells.rb
@@ -0,0 +1,19 @@
+require_relative '../environment.rb'
+
+class SPELLS
+
+    attr_accessor :name, :url
+
+    @@all = []
+
+    def initialize(name, url)
+        @name = name
+        @url = url
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end
