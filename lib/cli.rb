27678eb65e5c66f0680c9d04092dcfe8147c3f53
Erin Anderson <e.anderson1226@gmail.com>
2020-11-13 21:33:13 -0500

    required gem

--- lib/cli.rb
+++ lib/cli.rb
@@ -40,7 +40,6 @@ class CLI
             puts "#{index}. #{spells.name}"
         end
     end
-    binding.pry
 
     #have the user input the number corresponding to the spell they want to look at
     def spell_choice
